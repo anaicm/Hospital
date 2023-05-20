38e8febe562c40699b1d067dc3b14e3f342cb09c
anaicm <100219176+anaicm@users.noreply.github.com>
2023-05-20 15:18:33 +0200

    Pruebas

--- vista/pru.php
+++ vista/pru.php
@@ -1,34 +1,44 @@
 <?php
-echo 'hola';
-echo $_POST['username'];
-echo $_POST['password'];
-if(isset($_POST['login']))
-echo "Jan";
-else
- echo "June";
+require_once('../modelo/crud.php');
+require('../modelo/epecialista_agenda.php');
+//if(isset($_POST['fecha']) && isset( $_POST['nom_esp']) && isset( $_POST['nom_esp'])){
+        $fecha = '2023-05-31';
+        $nombre='Manuel';
+        $apellido='Ramírez López';
+        //if (isset($_POST['buscar_agendea_esp'])) {
+            $especialista=obtener_dni_usuario($nombre,$apellido);//obtengo el dni del especialista mediante su nombre y apellidos
+            foreach($especialista as $campos){
+              echo $dni=$campos['Dni'];
+              echo"<br>";
+            //}
+            $personal=id_Personal($dni);//con el dni obtengo el campo idPersonal
+            foreach($personal as $campo){
+               echo $idPersonal=$campo['idPersonal'];
+               echo"<br>";
+            }
+            try{
+                //$arreglo_fecha = date('Y-m-d', strtotime($fecha));
+                $busqueda=obtener_cita_por_fecha($fecha,$idPersonal);
+                echo "<table class='table table-hover'>";
+                echo "<thead><tr><th>Paciente</th><th></th><th>Fecha</th></tr></thead>";
+                echo "<tbody>";
+                foreach ($busqueda as $resultado) {
+                    echo ' <tr class="celdas">';
+                    echo "<td>" . $resultado['nombre'] ."</td>";
+                    echo "<td>" . $resultado['apellido'] ."</td>";
+                    echo "<td>" . $resultado['fecha'] ."</td>";
+                }
+                echo "</tbody>";
+                echo "</table>";
+                  
+            }catch(PDOException $e) {
+                echo 'Error en la búsqueda de la ciudad: ' . $e->getMessage();
+            } 
+        }
+   // } 
+
+
+
+
 
 ?>
-
-<div class="login_box">
-    <h2>Login</h2>
-    <?php if (!empty($msg)): ?>
-    <p><?php echo $msg; ?></p>
-    <?php endif; ?>
-    <form action="" method="post">
-        <div class="form_login">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" required>
-        </div>
-        <div class="form_login">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <div class="form_login button">
-            <input type="submit" id="login" name="login" value="Login">
-        </div>
-        <div class="contra_olvidar">
-            <p><a href="#">¿Has olvidado tú contraseña?</a></p>
-            <p><a href="index.php">Volver</a></p>
-        </div>
-    </form>
-</div>
